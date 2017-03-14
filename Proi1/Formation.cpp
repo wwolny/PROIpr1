e8c5173bb6c0d385ae3290c00823d93972ebf048
wwolny <wwolny1@gmail.com>
2017-03-14 12:30:09 +0000

    Formacja

---
+++ Proi1/Formation.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+class FORMATION{
+    private:
+    string type;
+    int formPower;
+    int formDefense;
+    int formCapacity;
+    int formSpeed;
+    enum={"Line", "Arrow", "Matrix"}
+    public:
+    void setFormPower(){
+
+    }
+
+}
